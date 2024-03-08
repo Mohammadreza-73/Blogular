32350a6cb91133d85b02f994119796933ed4076b
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2024-03-08 22:43:22 +0330

    Add Auth Module

---
+++ Modules/Auth/Providers/AuthServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Auth\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    private string $name = 'Auth';
+    private string $viewPath = '/../Resources/Views';
+
+    public function boot()
+    {
+        
+    }
+
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+
+        $this->loadViewFiles();
+    }
+
+    private function loadViewFiles(): void
+    {
+        $this->loadViewsFrom(__DIR__.$this->viewPath, $this->name);
+    }
+}
