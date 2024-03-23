9a21cbfcf3d7bc465919101b63984c42a81e340e
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2024-03-23 21:50:00 +0330

    Add dashboard layouts

--- Modules/Admin/Http/Controllers/DashboardController.php
+++ Modules/Admin/Http/Controllers/DashboardController.php
@@ -8,6 +8,6 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return 'hey admin';
+        return view('Admin::panel');
     }
 }
---
+++ Modules/Admin/Resources/Views/layouts/sidebar.blade.php
@@ -0,0 +1,289 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<!-- Brand Logo -->
+<a href" class="brand-link">
+    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+</a>
+
+<!-- Sidebar -->
+<div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+            <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+            <a href="#" class="d-block">{{ auth()->user()->name }}</a>
+        </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="{{ route('admin.panel') }}" class="nav-link">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <i class="nav-icon fas fa-calendar-alt"></i>
+                    <p>
+                    Calendar
+                    <span class="badge badge-info right">2</span>
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <i class="nav-icon far fa-image"></i>
+                    <p>
+                    Gallery
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <i class="nav-icon fas fa-columns"></i>
+                    <p>
+                    Kanban Board
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon far fa-envelope"></i>
+                    <p>
+                    Mailbox
+                    <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="pages/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Inbox</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="pages/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Compose</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="pages/mailbox/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Read</p>
+                    </a>
+                </li>
+            </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-book"></i>
+                    <p>
+                    Pages
+                    <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="pages/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Invoice</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="pages/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Profile</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="pages/examples/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>E-commerce</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="pages/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Projects</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="pages/examples/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Project Add</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="pages/examples/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Project Edit</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="pages/examples/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Project Detail</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="pages/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Contacts</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="pages/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>FAQ</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="pages/examples/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Contact us</p>
+                    </a>
+                </li>
+            </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon far fa-plus-square"></i>
+                    <p>
+                    Extras
+                    <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>
+                        Login & Register v1
+                        <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="pages/" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Login v1</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="pages/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Register v1</p>
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="pages/examples/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Forgot Password v1</p>
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="pages/examples/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Recover Password v1</p>
+                    </a>
+                    </li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>
+                    Login & Register v2
+                    <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                    <a href="pages/examples/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Login v2</p>
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="pages/examples/" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Register v2</p>
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="pages/examples/forgot-" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Forgot Password v2</p>
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="pages/examples/recover-" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Recover Password v2</p>
+                    </a>
+                    </li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                <a href="pages/" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Lockscreen</p>
+                </a>
+                </li>
+                <li class="nav-item">
+                <a href="pages/examples/legacy-" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Legacy User Menu</p>
+                </a>
+                </li>
+                <li class="nav-item">
+                <a href="pages/examples/" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Language Menu</p>
+                </a>
+                </li>
+                <li class="nav-item">
+                <a href="pages/" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Error 404</p>
+                </a>
+                </li>
+                <li class="nav-item">
+                <a href="pages/" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Error 500</p>
+                </a>
+                </li>
+                <li class="nav-item">
+                <a href="pages/" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Pace</p>
+                </a>
+                </li>
+                <li class="nav-item">
+                <a href="pages/" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Blank Page</p>
+                </a>
+                </li>
+                <li class="nav-item">
+                <a href" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Starter Page</p>
+                </a>
+                </li>
+            </ul>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+</div>
+<!-- /.sidebar -->
+</aside>
